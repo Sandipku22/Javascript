e0a69f812ac62482e01141328838ab40fdf4fb82
Sandipku22 <59016110+Sandipku22@users.noreply.github.com>
2025-08-03 04:03:18 +0000

    practice

---
+++ 01_basics/test.js
@@ -0,0 +1,12 @@
+
+"use strict";
+let userName = "Sandip";
+const userId = 123456;
+let userEmail = "sandip@gmail.com"
+let isloggedIn = null;
+
+
+userName = Number(userName);
+
+//console.table([typeof userName,typeof userId,typeof isloggedIn])
+console.table([typeof userName,userName]);
